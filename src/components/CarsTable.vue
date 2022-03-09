75aeed7422b089344cce35f59f5d1be4dc80a0c3
maciejaroslaw <48844889+maciejaroslaw@users.noreply.github.com>
2022-03-09 08:41:12 +0100

    adding pagination

--- src/components/CarsTable.vue
+++ src/components/CarsTable.vue
@@ -1,39 +1,50 @@
 <template>
     <div class="grid grid-cols-3 gap-4">
-            <div v-for="car in cars" :key="car.id" class="card group">
-                <header class="card-header">
-                    <!-- <p class="card-header-title">
-                    Component
-                    </p>
-                    <button class="card-header-icon" aria-label="more options">
-                    <span class="icon">
-                        <i class="fas fa-angle-down" aria-hidden="true"></i>
-                    </span>
-                    </button> -->
-                </header>
-                <div class="card-content pt-4 pl-4">
-                    <div class="content">
-                        <div>
-                            <div class="text-3xl">
-                                {{car.producer}}
-                                <span class="text-lg">{{car.model}}</span>
+        <div v-for="car in cars" :key="car.id" class="card group">
+            <header class="card-header">
+                <!-- <p class="card-header-title">
+                Component
+                </p>
+                <button class="card-header-icon" aria-label="more options">
+                <span class="icon">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                </span>
+                </button> -->
+            </header>
+            <div class="card-content pt-4 pl-4">
+                <div class="content">
+                    <div>
+                        <div class="text-3xl">
+                            {{car.producer}}
+                            <span class="text-lg">{{car.model}}</span>
+                        </div>
+                        <div class="mt-5">
+                            <div>
+                                Year: <span>{{car.year}}</span>
                             </div>
-                            <div class="mt-5">
-                                <div>
-                                    Year: <span>{{car.year}}</span>
-                                </div>
-                                <div class="mt-2">
-                                    Mileage: <span>{{car.mileage}}</span>km
-                                </div>
+                            <div class="mt-2">
+                                Mileage: <span>{{car.mileage}}</span>km
                             </div>
                         </div>
                     </div>
                 </div>
-                <footer class="card-footer opacity-0 group-hover:opacity-100 duration-300">
-                    <a href="#" class="card-footer-item">Edit</a>
-                    <a href="#" class="card-footer-item">Delete</a>
-                </footer>
             </div>
+            <footer class="card-footer opacity-0 group-hover:opacity-100 duration-300">
+                <a href="#" class="card-footer-item">Edit</a>
+                <a href="#" class="card-footer-item">Delete</a>
+            </footer>
+        </div>
+    </div>
+    <div>
+        <nav class="pagination" role="navigation" aria-label="pagination">
+            <a class="pagination-previous">Previous</a>
+            <a class="pagination-next">Next page</a>
+            <ul class="pagination-list">
+                <li v-for="p in pages" :key="p">
+                    <a class="pagination-link is-current" aria-label="Page 1" aria-current="page">{{p}}</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </template>
 
@@ -47,13 +58,17 @@ const props = defineProps({
 let page = ref(1);
 let elements = ref(9);
 
+let pages = computed(() => {
+    return Math.ceil(props.cars / elements);
+})
+
 let cars = computed(() => {
     const start = page.value * elements.value - elements.value;
     const end = start + elements.value - 1;
 
     return props.cars.filter((item, index) => index >= start && index <= end)
-   return props.cars
 })
+
 
 onMounted(() => {
     console.log(process.env.VUE_APP_API_URL);
