1078811ab9c3c3a34168c069b391a8549e9724ed
unknown <maciejaroslaw@gmail.com>
2022-03-09 19:25:06 +0100

    add adding editing and deleting cars

--- src/components/CarsTable.vue
+++ src/components/CarsTable.vue
@@ -30,13 +30,42 @@
                 </div>
             </div>
             <footer class="card-footer opacity-0 group-hover:opacity-100 duration-300">
-                <a href="#" class="card-footer-item">Edit</a>
-                <a href="#" class="card-footer-item">Delete</a>
+                <a @click="editCar(car)" href="#" class="card-footer-item">Edit</a>
+                <a @click="delCar(car)" href="#" class="card-footer-item">Delete</a>
             </footer>
         </div>
+        <div v-show="page == 1" class="card add-car self-start">
+            <header class="card-header">
+                <!-- <p class="card-header-title">
+                Component
+                </p>
+                <button class="card-header-icon" aria-label="more options">
+                <span class="icon">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                </span>
+                </button> -->
+            </header>
+            <div class="card-content pt-4 pl-4">
+                <div class="content">
+                    <div>
+                        <div class="text-3xl">
+                            Hey you...
+                            <span class="text-lg">want to add...</span>
+                        </div>
+                        <div class="mt-5">
+                            <div>
+                            </div>
+                            <div class="mt-2 flex justify-end">
+                                <button @click="emit('addCar')" class="button is-medium is-link">Some cars?</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </div>
     </div>
-    <div class="mt-2">
-        <nav class="pagination" role="navigation" aria-label="pagination">
+    <div class="mt-4 px-4">
+        <nav class="pagination is-right" role="navigation" aria-label="pagination">
             <a class="pagination-previous">Previous</a>
             <a class="pagination-next">Next page</a>
             <ul class="pagination-list">
@@ -56,9 +85,14 @@ const props = defineProps({
 });
 
 let page = ref(1);
-let elements = ref(9);
+let elements = computed(()=>{
+    return page.value == 1 ? 8 : 9;
+});
 
 let pages = computed(() => {
+    if(page.value === 1){
+        return Math.ceil(props.cars.length / elements.value - 1);
+    }
     return Math.ceil(props.cars.length / elements.value);
 })
 
@@ -71,6 +105,15 @@ let cars = computed(() => {
 
 function goToPage(p){
     page.value = p;
+}
+
+const emit = defineEmits(['editCar', 'delCar', 'addCar']);
+
+function editCar(car){
+    emit('editCar', car);
+}
+function delCar(car){
+    emit('delCar', car);
 }
 
 
@@ -86,5 +129,8 @@ onMounted(() => {
 //     display: grid;
 //     grid-template-columns: 1fr 1fr 1fr;
 // }
+.card{
+    background-image: linear-gradient(45deg, rgba(14, 14, 14,0.03) 0%, rgba(14, 14, 14,0.03) 38%,rgba(250, 250, 250,0.03) 38%, rgba(250, 250, 250,0.03) 45%,rgba(113, 113, 113,0.03) 45%, rgba(113, 113, 113,0.03) 100%),linear-gradient(135deg, rgba(148, 148, 148,0.03) 0%, rgba(148, 148, 148,0.03) 36%,rgba(219, 219, 219,0.03) 36%, rgba(219, 219, 219,0.03) 63%,rgba(62, 62, 62,0.03) 63%, rgba(62, 62, 62,0.03) 100%),linear-gradient(90deg, rgb(255,255,255),rgb(255,255,255));
+}
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,99 @@
 <template>
-  <div>
-    <cars-table :cars="cars" />
-  </div>
+<div>
+    <cars-table @addCar="modalAddCar = true" @delCar="handleDel" @editCar="handleEdit" :cars="cars" />
+    <div class="modal edit-car-modal is-clipped" :class="[modalEditCar ? 'is-active': '']">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+          <div class="box">
+            <div class="block content">
+                <div>
+                  <label class="label">Producer</label>
+                  <input class="input is-large" type="text" v-model="carToEdit.producer" placeholder="Large input">
+                </div>
+                <div class="mt-4">
+                  <label class="label">Model</label>
+                  <input class="input is-normal w-1/2" type="text" v-model="carToEdit.model" placeholder="Normal input">
+                </div>
+                <div class="mt-4">
+                 <label class="label">Year of production</label>
+                 <input class="input is-normal w-1/2" type="number" v-model="carToEdit.year" placeholder="Normal input">
+                </div>
+                <div class="mt-4">
+                  <label class="label">Mileage(km)</label>
+                  <input class="input is-normal w-1/2" type="number" v-model="carToEdit.mileage" placeholder="Normal input">
+                </div>
+                <div class="mt-4">
+                  <label class="label">Date of entry to ArsCars</label>
+                  <input class="input is-normal w-1/2" type="date" v-model="carToEdit.date_of_entry" placeholder="Normal input">
+                </div>
+                <div>
+                  <div class="is-flex is-justify-content-space-between mt-4">
+                      <button @click.prevent="modalEditCar = false" class="button is-danger">Discard changes</button>
+                      <button @click.prevent="editCar" class="button is-success">Save</button>
+                  </div>
+                </div>
+            </div>
+          </div>
+      </div>
+      <button class="modal-close is-large" @click="modalEditCar = false" aria-label="close"></button>
+    </div>
+    <div class="modal add-car-modal is-clipped" :class="[modalAddCar ? 'is-active': '']">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+          <div class="box">
+            <div class="block content">
+                <div>
+                  <label class="label">Producer</label>
+                  <input class="input is-large" type="text" v-model="newCar.producer">
+                </div>
+                <div class="mt-4">
+                  <label class="label">Model</label>
+                  <input class="input is-normal w-1/2" type="text" v-model="newCar.model">
+                </div>
+                <div class="mt-4">
+                <label class="label">Year of production</label>
+                <input class="input is-normal w-1/2" type="number" v-model="newCar.year" placeholder="2015">
+                </div>
+                <div class="mt-4">
+                  <label class="label">Mileage(km)</label>
+                  <input class="input is-normal w-1/2" type="number" v-model="newCar.mileage" >
+                </div>
+                <div class="mt-4">
+                  <label class="label">Date of entry to ArsCars</label>
+                  <input class="input is-normal w-1/2" type="date" v-model="newCar.date_of_entry">
+                </div>
+                <div>
+                  <div class="is-flex justify-center mt-4">
+                      <!-- <button @click.prevent="modalAddCar = false" class="button is-danger"></button> -->
+                      <button @click.prevent="addCar" class="button is-success ">Save</button>
+                  </div>
+                </div>
+            </div>
+          </div>
+      </div>
+      <button class="modal-close is-large" @click="modalAddCar = false" aria-label="close"></button>
+    </div>
+    <div class="modal delete-task-modal is-clipped" :class="[modalDelCar ? 'is-active': '']">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+          <div class="box">
+              <div class="block content">
+                  <h1>Delete this car?</h1>
+                  <div class="is-flex is-justify-content-space-between mt-4">
+                      <button @click.prevent="modalDelCar = !modalDelCar" class="button is-danger">No</button>
+                      <button @click.prevent="deleteCar" class="button is-success">Yes</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <button @click="modaDeleteTask = !modaDeleteTask" class="modal-close is-large" aria-label="close"></button>
+    </div>
+</div>
 </template>
 
 <script setup>
 import CarsTable from '../components/CarsTable.vue';
-import { ref, inject, onMounted} from 'vue';
+import { ref, reactive, inject, onMounted} from 'vue';
 import {useStore} from 'vuex';
 
 const axios = inject('axios');
@@ -15,6 +102,31 @@ const api_url = inject('api_url');
 const store = useStore();
 
 const cars = ref([]);
+
+const modalEditCar = ref(false);
+const modalDelCar = ref(false);
+const modalAddCar = ref(false);
+
+const newCar = reactive({
+  producer: '',
+  model: '',
+  year: Number,
+  mileage: 0,
+  date_of_entry: '',
+});
+
+const carToEdit = reactive({
+  id: 0,
+  producer: '',
+  model: '',
+  year: 0,
+  mileage: 0,
+  date_of_entry: '',
+});
+let carToDelete = reactive({
+  id: 0,
+})
+
 
 function getCars(){
   if(store.state.auth.user){
@@ -25,6 +137,87 @@ function getCars(){
     })
   }
 }
+
+function handleEdit(car){
+  carToEdit.id = car.id
+  carToEdit.producer = car.producer;
+  carToEdit.model = car.model;
+  carToEdit.year = car.year;
+  carToEdit.mileage = car.mileage;
+  carToEdit.date_of_entry = car.date_of_entry;
+
+  modalEditCar.value = true;
+};
+function handleDel(car){
+  modalDelCar.value = true;
+  carToDelete.id = car.id
+}
+
+
+function addCar(){
+  let data = new Date();
+  let date_of_entry = new Date(newCar.date_of_entry)
+  console.log(date_of_entry);
+  if(newCar.producer === ''){
+    store.dispatch("err/setError", "producer cannot be empty value")
+  }else if(newCar.model === ""){
+    store.dispatch("err/setError", "model cannot be empty value")
+  }else if(newCar.year < 1900 || newCar.year > data.getFullYear()){
+    store.dispatch("err/setError", "wrong year of production");
+  }else if(newCar.mileage < 0){
+    store.dispatch("err/setError", "Mileage cannot be lower than 0!");
+  }else if(isNaN(date_of_entry.getTime()) || date_of_entry.getTime() >= data.getTime() || date_of_entry.getFullYear() < 1970){
+    store.dispatch("err/setError", "wrong date of entry");
+  }else{
+    axios.post(`${api_url}/cars`, {
+      newCar,
+    }, {headers: token()}).then(res=>{
+      modalAddCar.value = false;
+      store.dispatch("err/setError", "You added new Car!");
+      getCars();
+    }).catch(err=>{
+      console.log(err);
+      store.dispatch("err/setError", err.response.data.message);
+    })
+  }
+};
+function editCar(){
+  let data = new Date();
+  let date_of_entry = new Date(carToEdit.date_of_entry)
+  console.log(date_of_entry.getTime());
+  if(carToEdit.producer === ''){
+    store.dispatch("err/setError", "producer cannot be empty value")
+  }else if(carToEdit.model === ""){
+    store.dispatch("err/setError", "model cannot be empty value")
+  }else if(carToEdit.year < 1900 || carToEdit.year > data.getFullYear()){
+    store.dispatch("err/setError", "wrong year of production");
+  }else if(carToEdit.mileage < 0){
+    store.dispatch("err/setError", "Mileage cannot be lower than 0!");
+  }else if(date_of_entry.getTime() > data.getTime() || date_of_entry.getFullYear() < 1970){
+    store.dispatch("err/setError", "wrong date of entry");
+  }else{
+    axios.put(`${api_url}/cars/${carToEdit.id}`, {
+      carToEdit,
+    }, {headers: token()}).then(res=>{
+      modalEditCar.value = false;
+      getCars();
+    }).catch(err=>{
+      console.log(err);
+      store.dispatch("err/setError", err.response.data.message);
+    })
+  }
+};
+function deleteCar(){
+  axios.delete(`${api_url}/cars/${carToDelete.id}`, {headers: token()}).then(res=>{
+    modalDelCar.value = false;
+    console.log(res);
+    getCars();
+  }).catch(err=>{
+    store.dispatch("err/setError", err.response.data.message);
+  })
+}
+
+
 
 onMounted(() => {
   getCars();
